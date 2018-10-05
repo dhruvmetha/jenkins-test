c7e859248aad41f325c64a4458310a46396a2575
dhruvmetha <dhruv.metha@gmail.com>
2018-10-05 17:30:33 +0530

    removed second srg from build

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -x
 
 docker network create --driver bridge mynet
 
-docker build -t mongo-image -f ./mongo
+docker build -t mongo-image ./mongo
 docker run -u root -d --network=mynet --name mongo -p 27017:27017 mongo-image
 
-docker build -t node-image -f ./node
+docker build -t node-image ./node
 docker run -u root -d --network=mynet --name node -p 3000:3000 -p 443:443 node-image
 
 docker network inspect mynet
