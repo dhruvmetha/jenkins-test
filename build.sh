08f6fd212760fcd83d0a88ff0bf6e48fb172e028
dhruvmetha <dhruv.metha@gmail.com>
2018-10-23 16:05:44 +0530

    changed node port

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker build -t mongo-image ./mongo
 docker run -u root -d --network=mynet --name mongo -p 27017:27017 mongo-image
 
 docker build -t node-image ./node
-docker run -u root -d --network=mynet --name node -p 3000:3000 -p 443:443 node-image
+docker run -u root -d --network=mynet --name node -p 3001:3001 -p 443:443 node-image
 
 docker build -t selenium-image ./selenium
 docker run -u root -d --network=mynet --name selenium selenium-image
